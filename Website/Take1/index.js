ca54b210cdfd27d2e5f839bb760b86c7d043860b
cbethin <bethin.charles@gmail.com>
2017-09-20 15:42:21 -0400

    website update

--- Website/Take1/index.js
+++ Website/Take1/index.js
@@ -1,20 +1,13 @@
 const HTTPS_PORT = 3000;
-const HTTP_PORT = 3001;
 
-/******* Imports *********/
+/*******Start*********/
 
 const os = require('os');
 const nodeStatic = require('node-static');
 const https = require('https');
+const http = require('http');
 const socketIO = require('socket.io')
 const fs = require('fs');
-const webrtc = require('wrtc');
-
-var RTCPeerConnection     = webrtc.RTCPeerConnection;
-var RTCSessionDescription = webrtc.RTCSessionDescription;
-var RTCIceCandidate       = webrtc.RTCIceCandidate;
-
-/******* HTTP SERVER *********/
 
 const certOptions = {
   key: fs.readFileSync('key.pem'),
@@ -30,51 +23,40 @@ var app = https.createServer(certOptions, function(req, res) {
 // Add Socket.IO to server
 var io = socketIO.listen(app);
 
-/******* VARIABLES *********/
-
-// STUN/TURN Config Options
-const configOptions = {"iceServers": [{"url": "stun:stun.l.google.com:19302"},
-		      {"url": "turn:35.167.210.171:3478",
-				"username": "cbethin",
-				"credential": "bethin"}]
-      };
-
 // Rooms
 var rooms = {
-  "name": [{
-    "uuid": 0000,
-    "socket": "thisSocket"
-  }]
+  "name": {}
 };
-
-// MCU needed stuff
-var uuid = 0101010101;
-var peer1uuid = 0001010;
-var sendToPeerValue = -1;
 
 /********************************/
 /********** EVENTS **************/
 /********************************/
 
-// every socket is a client
 io.sockets.on('connection', function(socket) {
-    console.log("Connected.");
 
-    socket.on('hello', function(roomName, uuid) {
-      joinRoom(roomName, uuid, socket);
-    })
+      socket.on('here', function(uuid, roomName) {
+        console.log("Here from: ", uuid);
+        for (var i = 0; i < rooms[roomName].clients.length; i++) {
+          rooms[roomName].clients[i].socket.emit('here', uuid);
+          console.log("Send \'here\' to ", rooms[roomName].clients[i].socket.id);
+        }
+      })
 
-    socket.on('disconnection', function() {
-      console.log(socket.id, ' disconnected!')
-    });
+      socket.on('signal', function(message, destUuid, roomName) {
+        onSignal(message, socket, destUuid, roomName);
+      });
 
-    socket.on('log', function(message) {
-      console.log("Log: ", message);
-    })
+      socket.on('disconnection', function() {
+        console.log(socket.id, ' disconnected!')
+      });
 
-    socket.on('signal', function(message, myUuid, roomName) {
-      gotMessageFromClient(message, socket, roomName);
-    })
+      socket.on('disconnectServer', function(uuid, roomName) {
+        onDisconnect(uuid, roomName);
+      });
+
+      socket.on('join', function(uuid, roomName) {
+        onJoin(uuid, socket, roomName);
+      });
 });
 
 
@@ -82,138 +64,83 @@ io.sockets.on('connection', function(socket) {
 /******* FUNCTIONALITY **********/
 /********************************/
 
-
-// Joins a client to a room
-function joinRoom(roomName, clientUuid, socket) {
-
-  var thisRoom = getRoom(roomName);
-  var newPeerConnection = createPeerConnection(roomName, clientUuid, socket);
-  var peer = {
-    uuid: clientUuid,
-    socket: socket,
-    peerConnection: newPeerConnection
-  }
-  thisRoom.push(peer);
-  rooms[roomName] = thisRoom;
-
-  socket.emit('here', uuid);
-  //sendOffer(peer, socket, roomName);
-}
-
-
-
-// Respond to messages from a client
-function gotMessageFromClient(message, socket, roomName) {
-  var signal = JSON.parse(message);
-  var clientUuid = signal.uuid;
-
-  // Ignore messages from ourself
-  if(clientUuid == uuid) {
-    console.log("Received from self");
-    return;
+function onSignal(message, socket, destUuid, roomName) {
+  room = rooms[roomName];
+  console.log("roomName:", roomName);
+  for (var i = 0; i < room.clients.length; i++) {
+    if (room.clients[i].uuid == destUuid) {
+      console.log("Sending", message.type, " from ", socket.id, " to ", room.clients[i].uuid)
+      room.clients[i].socket.emit('signal', message, socket.id);
+    };
   };
-
-  var peer = getPeer(clientUuid, roomName);
-  console.log("Looking through message...");
-
-  // Respond accordingly
-  if(signal.type == "sdp") {
-      peer.peerConnection.setRemoteDescription(new RTCSessionDescription(signal.sdp)).then(function() {
-          // Only create answers in response to offers
-          if(signal.sdp.type == 'offer') {
-              console.log("Got offer")
-              peer.peerConnection.createAnswer().then(function(description) {
-                peer.peerConnection.setLocalDescription(description).then(function() {
-                    socket.emit('signal', JSON.stringify({'type': 'sdp', 'sdp': peer.peerConnection.localDescription, 'uuid': uuid}), peer.uuid, roomName);
-                    //serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
-                }).catch(errorHandler);
-              }).catch(errorHandler);
-          } else {
-            console.log("Got answer")
-          }
-      }).catch(errorHandler);
-  } else if(signal.type == "ice") {
-      peer.peerConnection.addIceCandidate(new RTCIceCandidate(signal.ice)).catch(errorHandler);
-      console.log("Signal Ice:", signal.ice);
-  }
+  //socket.broadcast.emit('signal', message, socket.id);
 }
 
-
-// Send an RTC offer to a client
-function sendOffer(peer, socket, roomName) {
-
-  peer.peerConnection.createOffer().then(function(description) {
-    peer.peerConnection.setLocalDescription(description).then(function() { // setAndSendDescription
-        socket.emit('signal', JSON.stringify({'type': 'sdp', 'sdp': peer.peerConnection.localDescription, 'uuid': uuid}), peer.uuid, roomName);
-        //serverConnection.send(JSON.stringify({'sdp': peerConnection.localDescription, 'uuid': uuid}));
-    }).catch(errorHandler);
-  }).catch(errorHandler);
-
-  console.log("Sent offer");
+function onDisconnect(uuid, roomName) {
+    console.log(uuid, "Disconnecting"); 
+    if(rooms[roomName]) {
+        var clientsInRoom = rooms[roomName].clients
+        console.log("0 ", clientsInRoom[0]);
+        console.log("1", clientsInRoom[1]);
+        for(var i = 0; i < clientsInRoom.length; i++) {
+           console.log("i:", i);
+           if (clientsInRoom[i].uuid == uuid) {
+              // If this is the client, just remove them from the room
+              clientsInRoom.splice(i, 1);
+              rooms[roomName].clients = clientsInRoom;
+           } else {
+              // If this isn't the client, let them know the other client is leaving
+              clientsInRoom[i].socket.emit('disconnectClient', uuid, roomName);
+              console.log("Sent disconnect")
+           }
+        }
+     }
 }
 
+function onJoin(uuid, socket, roomName) {
 
-
-/**************************/
-/********* HELPERS ********/
-/**************************/
-
-// Create peer connection 1
-function createPeerConnection(roomName, clientUuid, socket) {
-  console.log("Creating Peer Connection");
-
-  var newPeerConnection;
-  newPeerConnection = new RTCPeerConnection(configOptions);
-  newPeerConnection.onicecandidate = function(event) {
-    if(event.candidate != null) {
-        socket.emit('signal', JSON.stringify({'type': 'ice', 'ice': event.candidate, 'uuid': uuid}), clientUuid, roomName);
+   if (!rooms[roomName]) {
+    //If the room does not exist, create it
+    console.log(socket.id, " created new room with id:", roomName);
+    rooms[roomName] = {
+      clients: [{"uuid": uuid, "socket": socket}]
     }
-  };
+  } else if (rooms[roomName].clients.length === 1) {
+    // If rooms exist, and the most recent room only has one client,
+    // add this client to the room
+    clientsInThisRoom = rooms[roomName].clients
+    clientsInThisRoom.push({'uuid': uuid, 'socket': socket});
+    rooms[roomName].clients = clientsInThisRoom;
 
-  newPeerConnection.onaddstream = function(event) {
-    console.log('Received remote stream:', event.stream);
-    console.log(window.URL.createObjectURL(event.stream));
-  };
+    // open the room and send idetifier to each
+    // for (var i=0; i<2; i++) {
+    //   clientsInThisRoom[i].socket.join(roomName);
+    // }
 
-  return newPeerConnection;
-}
+    clientsInThisRoom[0].socket.emit('ready', true, 2);
+    clientsInThisRoom[1].socket.emit('ready', false, 2);
+    console.log(socket.id, " joined the room ", roomName);
 
+  } /*else if (rooms.length > 0 && rooms[rooms.length - 1] && rooms[rooms.length-1].clients.length === 2) {
+    clientsInThisRoom = rooms[rooms.length-1].clients
+    clientsInThisRoom.push({'uuid': uuid, 'socket': socket});
+    rooms[rooms.length-1].clients = clientsInThisRoom;
 
-// Returns the desired room, and creates it if necessary
-function getRoom(roomName) {
-  var thisRoom = [];
-
-  if (!rooms[roomName]) {
-    rooms[roomName] = thisRoom;
-  } else {
-    thisRoom = rooms[roomName];
-  }
-
-  return thisRoom;
-}
-
-// Retreive a peer from a room given the roomName and uuid of peer
-function getPeer(uuid, roomName) {
-
-  if (rooms[roomName]) {
-    for (var i=0; i < rooms[roomName].length; i++) {
-      if (rooms[roomName][i].uuid == uuid) {
-        return rooms[roomName][i];
-      }
+    // open the room and send idetifier to each
+    var room_id = "hello2";
+    for (var i=0; i<3; i++) {
+      clientsInThisRoom[i].socket.join(room_id);
     }
-  }
 
-  return "not found";
+    clientsInThisRoom[0].socket.emit('ready', true, 3);
+    clientsInThisRoom[1].socket.emit('ready', false, 3);
+    clientsInThisRoom[2].socket.emit('ready', false, 3);
+    console.log(socket.id, " joined the room now!");
+  }*/
 }
 
-
-// Others
 function log() {
   var array = ['Message from server:'];
   array.push.apply(array, arguments);
   socket.emit('log', array);
-}
-
-function errorHandler(error) {
-    console.log(error);
 }
