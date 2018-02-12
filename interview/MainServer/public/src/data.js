6acf687d19ec8b47c4652c266358a62ee79df028
Justin Trugman <jtrugman14@gmail.com>
2018-02-12 15:16:13 -0500

    Faculty Data
    Filled faculty data js file with name, email address, image

--- interview/MainServer/public/src/data.js
+++ interview/MainServer/public/src/data.js
@@ -21,7 +21,52 @@ const ECE_faculty = {
     }
     'Chandramouli': {
         name: 'Rajarathnam Chandramouli',
-        email: 'mouli@ieee.org',
+        email: 'rchandr1@stevens.edu',
         img: 'chandramouli.jpg'
+    }
+    'Suba': {
+        name: 'Suba Subbalakshmi',
+        email: 'ksubbala@stevens.edu',
+        img: 'suba.jpg'
+    }
+    'Guo': {
+        name: 'Yi Guo',
+        email: 'yguo1@stevens.edu',
+        img: 'guo.jpg'
+    }
+    'Ackland': {
+        name: 'Bryan Ackland',
+        email: 'backland@stevens.edu',
+        img: 'ackland.jpg'
+    }
+    'Dov': {
+        name: 'Dov Kruger',
+        email: 'dkruger@stevens.edu',
+        img: 'kruger.jpg'
+    }
+    'Negar': {
+        name: 'Negar Tavassolian',
+        email: 'ntavasso@stevens.edu',
+        img: 'negar.jpg'
+    }
+    'Sabau': {
+        name: 'Serban Sabau',
+        email: 'ssabau@stevens.edu',
+        img: 'sabau.jpg'
+    }
+    'Yu': {
+        name: 'Shucheng Yu',
+        email: 'syu19@stevens.edu',
+        img: 'yu.jpg'
+    }
+    'Victor': {
+        name: 'Victor Lawrence',
+        email: 'vlawrenc@stevens.edu',
+        img: 'victor.jpg'
+    }
+    'Mukund': {
+        name: 'Mukund Iyengar',
+        email: 'miyengar@stevens.edu',
+        img: 'mukund.jpg'
     }
 };
