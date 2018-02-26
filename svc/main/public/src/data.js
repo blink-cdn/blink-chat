526948ef3cac2e67f92c7d00853bd09765a4550c
Justin Trugman <jtrugman14@gmail.com>
2018-02-26 15:09:17 -0500

    New Directories
    Created a directory for blink.blinkcdn.com
    &
    svc.blinkcdn.com

---
+++ svc/main/public/src/data.js
@@ -0,0 +1,72 @@
+const ECE_faculty = {
+    'Yao': {
+        name: 'Yu-Dong Yao',
+        email: 'yyao@stevens.edu',
+        img: 'yao.png'
+    }
+    'Man': {
+        name: 'Hong Man',
+        email: 'hman@stevens.edu',
+        img: 'man.jpg'
+    }
+    'Li': {
+        name: 'Hongbin Li',
+        email: 'hli@stevens.edu',
+        img: 'li.png'
+    },
+    'Comaniciu': {
+        name: 'Cristina Comaniciu',
+        email: 'ccomanic@stevens.edu',
+        img: 'comaniciu.png'
+    }
+    'Chandramouli': {
+        name: 'Rajarathnam Chandramouli',
+        email: 'rchandr1@stevens.edu',
+        img: 'chandramouli.jpg'
+    }
+    'Suba': {
+        name: 'Suba Subbalakshmi',
+        email: 'ksubbala@stevens.edu',
+        img: 'suba.jpg'
+    }
+    'Guo': {
+        name: 'Yi Guo',
+        email: 'yguo1@stevens.edu',
+        img: 'guo.jpg'
+    }
+    'Ackland': {
+        name: 'Bryan Ackland',
+        email: 'backland@stevens.edu',
+        img: 'ackland.jpg'
+    }
+    'Dov': {
+        name: 'Dov Kruger',
+        email: 'dkruger@stevens.edu',
+        img: 'kruger.jpg'
+    }
+    'Negar': {
+        name: 'Negar Tavassolian',
+        email: 'ntavasso@stevens.edu',
+        img: 'negar.jpg'
+    }
+    'Sabau': {
+        name: 'Serban Sabau',
+        email: 'ssabau@stevens.edu',
+        img: 'sabau.jpg'
+    }
+    'Yu': {
+        name: 'Shucheng Yu',
+        email: 'syu19@stevens.edu',
+        img: 'yu.jpg'
+    }
+    'Victor': {
+        name: 'Victor Lawrence',
+        email: 'vlawrenc@stevens.edu',
+        img: 'victor.jpg'
+    }
+    'Mukund': {
+        name: 'Mukund Iyengar',
+        email: 'miyengar@stevens.edu',
+        img: 'mukund.jpg'
+    }
+};
