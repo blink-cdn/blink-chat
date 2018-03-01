95306ea15f6138bf01074a1b097a9fa6b514db54
cbethin <bethin.charles@gmail.com>
2018-02-28 19:49:52 -0500

    db turned on

--- interview/BroadcastServer/index.js
+++ interview/BroadcastServer/index.js
@@ -275,7 +275,7 @@ function setupMongoCollection() {
 }
 
 function stringifyStreamRoom() {
-    newStreamRoom = Object.assign({}, streamRooms);
+    newStreamRoom = $.extend(true, {}, streamRooms);
     for (roomName in newStreamRoom) {
         for (clientID in newStreamRoom[roomName].clients) {
             newStreamRoom[roomName].clients[clientID].socket = null;
@@ -283,42 +283,4 @@ function stringifyStreamRoom() {
     }
 
     return JSON.stringify(newStreamRoom);
-}
-
-
-////
-function isCyclic(obj) {
-    var keys = [];
-    var stack = [];
-    var stackSet = new Set();
-    var detected = false;
-
-    function detect(obj, key) {
-        if (typeof obj != 'object') { return; }
-
-        if (stackSet.has(obj)) { // it's cyclic! Print the object and its locations.
-            var oldindex = stack.indexOf(obj);
-            var l1 = keys.join('.') + '.' + key;
-            var l2 = keys.slice(0, oldindex + 1).join('.');
-            console.log('CIRCULAR: ' + l1 + ' = ' + l2 + ' = ' + obj);
-            console.log(obj);
-            detected = true;
-            return;
-        }
-
-        keys.push(key);
-        stack.push(obj);
-        stackSet.add(obj);
-        for (var k in obj) { //dive on the object's children
-            if (obj.hasOwnProperty(k)) { detect(obj[k], k); }
-        }
-
-        keys.pop();
-        stack.pop();
-        stackSet.delete(obj);
-        return;
-    }
-
-    detect(obj, 'obj');
-    return detected;
 }
