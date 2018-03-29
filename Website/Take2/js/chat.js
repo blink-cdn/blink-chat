0f6af44ec14125826e887d2757fca76facbf724a
root <root@ip-172-31-26-247.us-east-2.compute.internal>
2018-03-29 03:46:14 +0000

    ..

--- Website/Take2/js/chat.js
+++
@@ -1,8 +0,0 @@
-function join() {
-  var roomNameBox = document.getElementById("roomNameBox");
-  var roomName = roomNameBox.value;
-  var hostName = window.location.hostname;
-  console.log(hostName); 
-  window.location.href = "https://" + hostName + "/chatpage.html#" + roomName;
-
-}
